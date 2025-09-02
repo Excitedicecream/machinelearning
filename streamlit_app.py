ea7fd438c99a9264684bd17b0b534ff2466edf60
Excitedicecream <jwts10@gmail.com>
2025-09-02 16:07:47 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,32 @@
 import streamlit as st
 import pandas as pd
-from numpy.random import default_rng as rng
 
 st.title('Machine Learning App 🤖')
 
-st.info('This app build a machine learning model')
+st.info('This app builds a machine learning model')
+
+# Load data
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
 
 with st.expander('Data'):
-  st.write('**Raw Data**')
-  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
-  df
+    st.write('**Raw Data**')
+    st.dataframe(df)
 
-  st.write ('**X**')
-  X=df.drop('species', axis = 1)
-  X
+    st.write('**X**')
+    X = df.drop('species', axis=1)
+    st.dataframe(X)
 
-  st.write('**y**')
-  y=df.species
-  y
+    st.write('**y**')
+    y = df['species']
+    st.dataframe(y)
 
 with st.expander('Data Visualisation'):
-  with st.expander('Data Visualisation'):
-#"species","island","bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g","sex"
-  st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
+    st.write('**Scatter Plot**')
+
+    # Dropdown for X and Y axis
+    numeric_columns = df.select_dtypes(['float64', 'int64']).columns.tolist()
+    x_axis = st.selectbox('Select X-axis', options=numeric_columns, index=0)
+    y_axis = st.selectbox('Select Y-axis', options=numeric_columns, index=1)
+
+    # Plot scatter chart
+    st.scatter_chart(data=df, x=x_axis, y=y_axis, color='species')
