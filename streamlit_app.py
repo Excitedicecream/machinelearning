aab8a82202c1fd422bf8b11f76fda0b726727483
Excitedicecream <jwts10@gmail.com>
2025-09-02 16:09:36 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
 
 st.title('Machine Learning App 🤖')
 
@@ -21,18 +20,13 @@ with st.expander('Data'):
     y = df['species']
     st.dataframe(y)
 
-with st.expander('2D Data Visualisation'):
-    st.write('**2D Scatter Plot**')
+with st.expander('Data Visualisation'):
+    st.write('**Scatter Plot**')
+
+    # Dropdown for X and Y axis
     numeric_columns = df.select_dtypes(['float64', 'int64']).columns.tolist()
-    x_axis = st.selectbox('Select X-axis', options=numeric_columns, index=0, key='x_axis_2d')
-    y_axis = st.selectbox('Select Y-axis', options=numeric_columns, index=1, key='y_axis_2d')
+    x_axis = st.selectbox('Select X-axis', options=numeric_columns, index=0)
+    y_axis = st.selectbox('Select Y-axis', options=numeric_columns, index=1)
+
+    # Plot scatter chart
     st.scatter_chart(data=df, x=x_axis, y=y_axis, color='species')
-
-with st.expander('3D Data Visualisation'):
-    st.write('**3D Scatter Plot**')
-    col1 = st.selectbox('Select X-axis', options=numeric_columns, index=0, key='x_axis_3d')
-    col2 = st.selectbox('Select Y-axis', options=numeric_columns, index=1, key='y_axis_3d')
-    col3 = st.selectbox('Select Z-axis', options=numeric_columns, index=2, key='z_axis_3d')
-
-    fig = px.scatter_3d(df, x=col1, y=col2, z=col3, color='species', size_max=10)
-    st.plotly_chart(fig, use_container_width=True)
