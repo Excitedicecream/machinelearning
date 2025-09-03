af038903a3054ab4e9190ab327a2890b8eb3e8d9
Excitedicecream <jwts10@gmail.com>
2025-09-03 12:52:48 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,7 +74,7 @@ with st.expander('3D Data Visualisation'):
 with st.sidebar:
     st.header('Input Features')
     island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
-    sex = st.selectbox('sex', ('female', 'male'))
+    sex = st.selectbox('Sex', ('female', 'male'))
     bill_length_mm = st.slider('Bill Length (mm)', 32.1, 59.6, 43.9)
     bill_depth_mm = st.slider('Bill Depth (mm)', min_value=13.1, max_value=21.5, value=17.0)
     flipper_length_mm = st.slider('Flipper Length (mm)', min_value=172, max_value=231, value=200)
@@ -91,10 +91,15 @@ data = {
 }
 
 input_df = pd.DataFrame(data)
-input_penguins = pd.concat([input_df, X_raw], axis=0)
+input_penguins= pd.concat([input_df,X],axis=0)
 
 with st.expander('Input features'):
     st.write('**Input penguin**')
     input_df
     st.write('Combined penguin data**')
-    input_penguines
+    input_penguins
+
+#Encode
+encode = ['island', 'sex']
+df_penguins = pd.get_dummies(input_penguins, columns=encode)
+df_penguins
